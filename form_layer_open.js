9531ab1e55e782fa07c2ea158576bb58ce009faf
leepand <85721094@qq.com>
2019-01-18 14:03:13 +0800

    first commit

--- form_layer_open.js
+++ form_layer_open.js
@@ -33,3 +33,38 @@ $("body").on("click",".edit_desc",function(){
     });    
         
     })
+
+
+
+//content中id val的获取
+
+    $("body").on("click",".edit_desc",function(){
+        
+      var index =  layer.open({
+        id:1,
+        type: 1,
+        title:'Update Description',
+        skin:'layui-layer-rim',
+        area:['450px', 'auto'],
+            
+        content: ' <div class="row" style="width: 420px;  margin-left:7px; margin-top:10px;">'
+            +'<div class="col-sm-12">'
+            +'<div class="input-group">'
+            //+'<span class="input-group-addon"> 新 密 码   :</span>'
+            +'<textarea name="desc" placeholder="请输入内容" class="layui-textarea" id="form_edit_desc" style="width: 390px;"></textarea>'
+            +'</div>'
+            +'</div>'
+              +'</div>'
+        ,
+        btn:['保存描述','取消'],
+        btn1: function (index,layero) {
+         console.log('edit_desc',$("#form_edit_desc").val()) 
+        //post/get回调函数在此定义
+        },
+        btn2:function (index,layero) {
+             layer.close(index);
+        }
+ 
+    });    
+        
+    })
