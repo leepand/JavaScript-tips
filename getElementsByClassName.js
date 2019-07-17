b3ab92af3a14dba2ff0c980fb68755c9928d9c14
leepand <85721094@qq.com>
2019-07-17 15:53:37 +0800

    add getElementsByClassName.js

---
+++ getElementsByClassName.js
@@ -0,0 +1,38 @@
+toggle_visibility("sidenav-item")
+function toggle_visibility(className) {
+   var elements = getElementsByClassName(document, className),
+       n = elements.length;
+   for (var i = 0; i < n; i++) {
+     var e = elements[i];
+
+     if(e.style.display == 'block') {
+       e.style.display = 'none';
+     } else {
+       e.style.display = 'block';
+     }
+  }
+}
+
+
+function getElementsByClassName(node,classname) {
+    if (node.getElementsByClassName) { // use native implementation if available
+        return node.getElementsByClassName(classname);
+    } else {
+    return (function getElementsByClass(searchClass,node) {
+        if ( node == null )
+          node = document;
+        var classElements = [],
+            els = node.getElementsByTagName("*"),
+            elsLen = els.length,
+            pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)"), i, j;
+
+        for (i = 0, j = 0; i < elsLen; i++) {
+          if ( pattern.test(els[i].className) ) {
+              classElements[j] = els[i];
+              j++;
+          }
+        }
+        return classElements;
+    })(classname, node);
+  }
+}
