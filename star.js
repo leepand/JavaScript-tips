44a4a93f6f7defdd55807bd1c1737b4678ff89fa
leepand <85721094@qq.com>
2019-09-02 20:50:14 +0800

    add star.js

---
+++ star.js
@@ -0,0 +1,72 @@
+<div class="row">
+                                <div class="col-lg-8 order-2 order-lg-1">
+                                    <div class="ui-bordered mb-3">
+                                        <div class="p-4">
+                                            <!-- Avatar + Name -->
+                                            <img src="../static/assets/appwork/assets/assets_/img/avatars/1-small.png" class="ui-w-30 rounded-circle" alt=""> &nbsp;
+                                            <a href="javascript:void(0)">MJ</a> &nbsp;
+                                            <a href="javascript:void(0)" class="text-muted small">2 reviews</a>
+                                            <!-- Rating -->
+                                            <div class="mt-3">
+                                                <div class="ui-stars">
+                                                    <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                    <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                    <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                    <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                    <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                </div> &nbsp;
+                                                <span class="text-muted">Excellent</span>
+                                            </div>
+                                            <!-- Content -->
+                                            <h6 class="mt-4 mb-2">Pros</h6>
+                                            <p>comming soon.</p>
+                                            <!-- / Content -->
+                                            <!-- Votes -->
+                                            <a href="#" class="float-right text-muted small"><i class="ion ion-ios-thumbs-down"></i> 3</a>
+                                            <a href="#" class="float-right text-muted small mr-3"><i class="ion ion-ios-thumbs-up"></i> 12</a>
+                                            <!-- Date -->
+                                            <small class="text-muted">12 days ago</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 order-1 order-lg-2">
+                                    <div style="position: -webkit-sticky; position: sticky; top: 10px;">
+                                        <table class="table table-bordered">
+                                            <tbody>
+                                                <tr>
+                                                    <!-- Set columns width -->
+                                                    <!-- Five start -->
+                                                    <td class="text-center align-middle" style="width: 110px;">
+                                                        <div class="ui-stars">
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                        </div>
+                                                    </td>
+                                                    <td class="percents text-muted" style="width: 60px;">78%</td>
+                                                    <td><a href="#">78 reviews</a></td>
+                                                </tr>
+                                                <!-- Four stars -->
+                                                <tr>
+                                                    <td class="text-center align-middle">
+                                                        <div class="ui-stars">
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star filled"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                            <div class="ui-star"><i class="ion ion-md-star"></i><i class="ion ion-md-star"></i></div>
+                                                        </div>
+                                                    </td>
+                                                    <td class="percents text-muted">12%</td>
+                                                    <td><a href="#">12 reviews</a></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                        <button type="button" class="btn btn-default md-btn-flat text-expanded mb-4">
+                                            <small class="ion ion-md-create"></small> &nbsp; <small>WRITE REVIEW</small>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
