80147b46bdfaae1da775c49c169030ccf6e53e1d
leepand <85721094@qq.com>
2019-12-19 18:51:21 +0800

    update sort.js

--- sort.js
+++ sort.js
@@ -41,3 +41,59 @@
             });
         }
     }
+
+
+
+
+var willSort = [
+    {
+        name:'shangwenhe',
+        age:25,
+        height:170
+    },
+    {
+        name:'zhangsan',
+        age:31,
+        height:169
+    },
+    {
+        name:'lisi',
+        age:31,
+        height:167
+    },
+    {
+        name:'zhaowu',
+        age:22,
+        height:160
+    },
+    {
+        name:'wangliu',
+        age:23,
+        height:159
+    }
+];
+
+/*
+    @function     JsonSort 对json排序
+    @param        json     用来排序的json
+    @param        key      排序的键值
+*/
+function JsonSort(json,key){
+    //console.log(json);
+    for(var j=1,jl=json.length;j < jl;j++){
+        var temp = json[j],
+            val  = temp[key],
+            i    = j-1;
+        while(i >=0 && json[i][key]>val){
+            json[i+1] = json[i];
+            i = i-1;   
+        }
+        json[i+1] = temp;
+       
+    }
+    //console.log(json);
+    return json;
+
+}
+var json = JsonSort(willSort,'age');
+console.log(json);
